89669cf2f5086612011b47c0f26b61876be69c14
Michel Atieno <mishqamish@gmail.com>
2018-09-06 11:41:12 +0300

    Able to create profile and update, send welcome email to user

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 export MOVIE_API_KEY="f868ee53769996480189b6ac261112a0"
 export SECRET_KEY="Michel23"
 
+export MAIL_USERNAME="moringamelissa254@gmail.com"
+export MAIL_PASSWORD="moringa123"
+
 python3.6 manage.py server

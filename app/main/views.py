bcc4acf5155606aa57dbcb1b568e344f62abf149
Michel Atieno <mishqamish@gmail.com>
2018-09-06 14:40:50 +0300

    Final changes to file

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,7 @@ from .forms import ReviewForm,UpdateProfile
 from ..models import Review, User
 from flask_login import login_required, current_user
 from .. import db,photos
+import markdown2 
 
 # Views
 # Views
@@ -79,6 +80,14 @@ def new_review(id):
 
     title = f'{movie.title} review'
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
+
+@main.route('/review/<int:id>')
+def single_review(id):
+    review=Review.query.get(id)
+    if review is None:
+        abort(404)
+    format_review = markdown2.markdown(review.movie_review,extras=["code-friendly", "fenced-code-blocks"])
+    return render_template('review.html',review = review,format_review=format_review)
 
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
